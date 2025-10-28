33f7a18462385617ba0937683e341a89b1a2eb2b
ibn-h <ibnnh.deen@gmail.com>
2025-10-28 19:50:22 +0100

    Updated

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -4,3 +4,5 @@ export const APPLES_LIMIT = 10;
 export const APPLE_COLOR = "red";
 export const SNAKE_COLOR = "green";
 export const SNAKE_HEAD_COLOR = "darkgreen";
+
+export const URL = "https://snake-game-ygmm.onrender.com";
--- public/scripts/game/leaderboard.js
+++ public/scripts/game/leaderboard.js
@@ -1,5 +1,7 @@
+import { URL } from "../config.js";
+
 export async function fetchLeaderboard() {
-  const response = await fetch("http://localhost:3000/leaderboard");
+  const response = await fetch(`${URL}/leaderboard`);
 
   if (!response.ok) {
     console.error("Failed to fetch leaderboard");
@@ -12,7 +14,7 @@ export async function fetchLeaderboard() {
 export function submitScore(score) {
   const userID = localStorage.getItem("userID");
 
-  fetch("http://localhost:3000/submit-score", {
+  fetch(`${URL}/submit-score`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ id: userID, score: score }),
